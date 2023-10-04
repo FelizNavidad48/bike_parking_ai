a31904a0074074a597df65170c16670f4b6b8cd8
FelizNavidad48 <87990148+FelizNavidad48@users.noreply.github.com>
2023-10-04 14:30:43 +0200

    gitignore

---
+++ yolo/yolo.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+#from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+#import cv2
+
+# Load a model
+model = YOLO('yolov8x.pt')  # load an official model
+
+# Predict with the model
+#results = model.predict(source='parking.jpg', save=True, conf=0.2)  # predict on an image
+results = model.predict(source='test.jpg', save=True, conf=0.25)
+print(results)
